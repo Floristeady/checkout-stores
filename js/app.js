9dfee386747614689766a552425fa765082a281e
Florencia Rosenfeld <hola@floristeady.com>
2015-11-13 20:40:26 -0600

    checkout stores files

--- js/app.js
+++ js/app.js
@@ -1,3 +1,65 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+//$(document).foundation();
+
+/************************* 
+  Variables
+  **************************/
+jQuery(function($){ 
+  var browserwidth;
+  var desktopwidth=1024;
+  var mobilewidth=767;
+  
+/************************* 
+ Functions
+**************************/
+
+  function getbrowserwidth(){
+    browserwidth=window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth||0;
+    return browserwidth;
+  }
+  
+  function onLoadAndResize(){
+    radioAddClass();
+           
+    if(browserwidth>=mobilewidth){
+      heightForBackground();
+    }
+  }
+  
+  function heightForBackground(){ 
+	  heightSidebar = $('#sidebar').height();	  
+	  $('#main-content span.back').css('height', heightSidebar+20);
+  }
+  
+  function radioAddClass() {
+	  $('input:radio').click(function() {
+		  console.log('clic');
+		    if ($(this).is(':checked')) {
+		        $(this).parent().addClass('active');
+		        $(this).parent().siblings('label').removeClass('active');
+		    } else {
+		        $(this).parent().siblings('label').removeClass('active');
+		    };
+		});
+  }
+  
+  
+/************************* 
+ Execution
+**************************/
+
+  $(window).resize(function(){
+	 getbrowserwidth(); 
+     onLoadAndResize();
+     
+   });
+   
+   
+  $(document).ready(function(){
+	getbrowserwidth();   
+    onLoadAndResize();
+           
+   });
+  
+});
